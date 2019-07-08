eb632ab611f297f304caabba8762ae8e58131206
santosgagbegnon <sgagb074@uottawa.ca>
2019-07-08 16:58:43 -0400

    Fixed mapbox

--- cuHacking/Map/MapViewController.swift
+++ cuHacking/Map/MapViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 import Mapbox
 
 class MapViewController : CUViewController{
+    private var mapView : MGLMapView!
     override func viewDidLoad() {
-//        let url = URL(string: "mapbox://styles/mapbox/light-v9")
-//        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
-//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
-//        view.addSubview(mapView)
+        super.viewDidLoad()
+        let url = traitCollection.userInterfaceStyle == .light ? MGLStyle.lightStyleURL :  MGLStyle.darkStyleURL
+        mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 45.3823547, longitude: -75.6974599), zoomLevel: 17, animated: false)
+        view.addSubview(mapView)
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if #available(iOS 13.0, *) {
+            if(previousTraitCollection?.hasDifferentColorAppearance(comparedTo: traitCollection) == false){
+                return
+            }
+            let url = traitCollection.userInterfaceStyle == .light ? MGLStyle.lightStyleURL :  MGLStyle.darkStyleURL
+            mapView.styleURL = url
+        }
     }
 }
